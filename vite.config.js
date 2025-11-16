f27a19946c86dd74b545a50dad7d53b0574f93e3
Petar Simic <petar.simic@yahoo.com>
2025-11-16 14:06:17 +0100

    config for vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 // Set base to the repository name so assets are loaded correctly on GitHub Pages
 export default defineConfig({
-  base: '/cash-cow-like/',
-})
+    base: '/cash-cow-like/',
+});
